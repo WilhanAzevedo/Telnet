33d2b8ca594416d8b178c6908ab98c3807f196d6
Wilhan Azevedo <wilhan.azevedo@gmail.com>
2020-03-06 08:04:58 -0400

    Beta

--- dtc.php
+++ dtc.php
@@ -2,15 +2,9 @@
 namespace Telnet;
 include ('TelnetClient.php');
 
-//use TelnetClient;
-
-//Uncomment this to get debug logging
-//TelnetClient::setDebug(true);
-
 $telnet = new TelnetClient('172.16.13.242', 23);
 $telnet->connect();
-$telnet->setPrompt('#'); //setRegexPrompt() to use a regex
-//$telnet->setPruneCtrlSeq(true); //Enable this to filter out ANSI control/escape sequences
+$telnet->setPrompt('#');
 $telnet->login('admin', 'dlpatm197');
 $sair = 'sair';
 
@@ -19,6 +13,7 @@ $comando = readline();
 $cmdResult = $telnet->execute($comando);
 echo($cmdResult);
 }while($comando != $sair);
-
 $telnet->disconnect();
+
+
 ?>
